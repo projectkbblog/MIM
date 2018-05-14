e0d0004a2f045e242853301f0ca760bdafbde129
Project KB Blog <projectkbblog@outlook.com>
2018-05-15 09:21:27 +1000

    password expiration script
    addition of script to get the password expiration date and time of an AD account

---
+++ Scripts/Operational/Active Directory/Get-AccountPasswordExpiration.ps1
@@ -0,0 +1,21 @@
+﻿#####
+#
+# Script that will query AD and return the date and time the user's password is due to expire.
+# 
+# Sample Usage:
+#     - Get-AccountPasswordExpiration.ps1 -AccountName bsmith
+#
+# Author: Andrew Silcock
+# Date Created: 15-May-2018
+# Version: 0.1
+#
+#####
+param
+(
+    [parameter(Mandatory=$true)]
+    [string] $AccountName
+)
+
+
+$User = Get-ADUser $AccountName -Properties 'msDS-UserPasswordExpiryTimeComputed'
+[datetime]::FromFileTime($User."msDS-UserPasswordExpiryTimeComputed")
