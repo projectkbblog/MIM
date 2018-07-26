c077f76f63d3e80f82f96752737aee7caf948d1b
Project KB Blog <projectkbblog@outlook.com>
2018-07-27 09:08:14 +1000

    AD password change script
    commit script to change a user's AD password via PowerShell

---
+++ Scripts/Operational/Active Directory/Password Change/PasswordChange.ps1
@@ -0,0 +1,21 @@
+﻿#####
+#
+# Script that allows you to change your AD password whilst logged onto an RDP session (often where you can't get to ALT-CTRL-DEL change password).
+# 
+# The username of the account is specified as a parameter, however the script will then prompt you to enter the current and new password.
+#
+# Sample Usage:
+#     - PasswordChange.ps1 -AccountName bsmith
+#
+# Author: Andrew Silcock
+# Date Created: 27-Jul-2018
+# Version: 0.1
+#
+#####
+param
+(
+    [Parameter(Mandatory=$true)]
+    [string] $AccountName
+)
+
+Set-ADAccountPassword -Identity $AccountName -OldPassword (Read-Host "Enter your old password" -AsSecureString) -NewPassword (Read-Host "Enter your new password" -AsSecureString)
