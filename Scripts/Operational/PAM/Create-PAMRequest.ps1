35a536c4d84812eb0aec5c390a489173b84161ef
Project KB Blog <projectkbblog@outlook.com>
2018-09-12 10:38:42 +1000

    MIM PAM helper scripts
    Initial commit of helper scripts for interacting with the MIM PAM solution via the PowerShell cmdlets.

---
+++ Scripts/Operational/PAM/Create-PAMRequest.ps1
@@ -0,0 +1,92 @@
+﻿param 
+(
+    [Parameter(Mandatory=$true)]
+    [string] $RoleName,
+    [Parameter(Mandatory=$true)]
+    [string] $Justification,
+    [Parameter(Mandatory=$false)]
+    [TimeSpan] $TimeSpan,
+    [Parameter(Mandatory=$false)]
+    [Datetime] $RequestedTime
+)
+#####
+#
+# Script that utlises the MIM PAM Powershell cmdlets to create a PAM Request
+# 
+# Pre-requisites:
+#   - the script must be run on a server or desktop with the MIM PAM PowerShell cmdlets installed
+#   - it is assumed the user running the script is eligible for escalation in the specified role
+#
+# Sample Usage:
+#
+#  - Request access to the MIM Operators role for 2 hours, starting in 2 hours time
+#      .\Create-PAMRequest -RoleName "MIM Operators" -Justification "Access required to administer the system" -TimeSpan (New-TimeSpan -Hours 2) -RequestedTime (Get-Date).AddHours(2)
+#
+#  - Request access to the MIM Operators role for 2 hours, starting as as soon as the request is approved
+#     .\Create-PAMRequest -RoleName "MIM Operators" -Justification "Access required to administer the system" -TimeSpan (New-TimeSpan -Hours 2)
+#
+#  - Request access to the MIM Operators role for the default period of time, starting as as soon as the request is approved
+#      .\Create-PAMRequest -RoleName "MIM Operators" -Justification "Access required to administer the system" 
+#
+#  - Request access to the MIM Operators role for the default period of time, starting in 2 days time
+#      .\Create-PAMRequest -RoleName "MIM Operators" -Justification "Access required to administer the system" -RequestedTime (Get-Date).AddDays(2)#
+#
+#
+# Author: Andrew Silcock
+# Date Created: 10-Nov-2017
+# Version: 0.1
+#
+#####
+
+function Get-PAMRole
+{
+    param 
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $Role 
+    )
+
+    try
+    {
+        $PAMRole = Get-PAMRoleForRequest -DisplayName $Role 
+    }
+    catch { return $null }
+
+    return $PAMRole
+}
+$RoleForApproval = Get-PAMRole -Role $RoleName
+
+if ($TimeSpan -and $TimeSpan -gt (New-TimeSpan -seconds $RoleForApproval.TTL))
+{
+    Write-Warning ("The requested time span '{0}' is greate than the maximum allowable '{1}'" -f $TimeSpan,  (New-TimeSpan -seconds $RoleForApproval.TTL))
+    exit 0
+}
+
+if ($RoleForApproval)
+{
+    if ($TimeSpan -and $RequestedTime)
+    {
+        "Creating Role Request`n`tRole:`t{0}`n`tJustification:`t{1}`n`tRequested Time Span:`t{2}`n`tRequested Start Time:`t{3}" -f $RoleName, $Justification, $TimeSpan, $RequestedTime
+        New-PAMRequest -Role $RoleForApproval -Justification $Justification -RequestedTTL $TimeSpan -RequestedTime $RequestedTime
+    }
+    elseif ($TimeSpan)
+    {
+        "Creating Role Request`n`tRole:`t{0}`n`tJustification:`t{1}`n`tRequested Time Span:`{2} (mins)`n`tRequested Start Time:`t{2}" -f $RoleName, $Justification, $TimeSpan, (Get-Date)
+        New-PAMRequest -Role $RoleForApproval -Justification $Justification -RequestedTTL $TimeSpan
+    }
+    elseif ($RequestedTime)
+    {
+        "Creating Role Request`n`tRole:`t{0}`n`tJustification:`t{1}`n`tRequested Time Span:`t{2} (default)`n`tRequested Start Time:`t{3}" -f $RoleName, $Justification, $RoleForApproval.TTL, $RequestedTime
+        New-PAMRequest -Role $RoleForApproval -Justification $Justification -RequestedTime $RequestedTime
+    }
+    else
+    {
+        "Creating Role Request`n`tRole:`t{0}`n`tJustification:`t{1}`n`tRequested Time Span:`t{2} (default)`n`tRequested Start Time:`t{3}" -f $RoleName, $Justification, $RoleForApproval.TTL, (Get-Date)
+        New-PAMRequest -Role $RoleForApproval -Justification $Justification
+    }
+}
+else
+{
+    Write-Warning "Role '{0}' not found" -f $RoleName
+
+}
---
+++ Scripts/Operational/PAM/Get-PAMApprovals.ps1
@@ -0,0 +1,110 @@
+﻿#####
+#
+# Script that utlises the MIM PAM Powershell cmdlets to retrieve any escalation requests that are approved by the current user and prompts them for an action (Approve, Deny or Ignore).
+# 
+# Pre-requisites:
+#   - the script must be run on a server or desktop with the MIM PAM PowerShell cmdlets installed
+#   - it is assumed the user running the script is an approver for escalation requests.
+#
+# Sample Usage:
+#     View pending approvals and be prompted for the action to take
+#     - .\Get-PAMApprovals.ps1
+#
+# Author: Andrew Silcock
+# Date Created: 10-Nov-2017
+# Version: 0.1
+#
+#####
+
+function Get-ApprovalAction
+{
+    ##########
+    # Function: Get-ApprovalAction 
+    #
+    # Presents a menu to the user running the scrip as to whether they want to Approve, Deny or Ignore the request
+    #
+    ##########
+
+    $title = "Approve the request"
+    $message = "Do you want to approve this request?"
+
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Approve", `
+        "Approves the request"
+
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&Deny", `
+        "Denies the request"
+
+    $ignore = New-Object System.Management.Automation.Host.ChoiceDescription "&No Action", `
+        "Takes no action with request"
+
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no, $ignore)
+
+    $result = $host.ui.PromptForChoice($title, $message, $options, 0) 
+
+    switch ($result)
+        {
+            0 {return "approve"}
+            1 {return "deny"}
+            2 {return "ignore"}
+        }
+    return "ignore"
+}
+
+# Gets PAM Approvals requiring action
+$Requests = Get-PAMRequestToApprove
+"There are {0} requests to approve" -f $Requests.Length
+""
+
+$counter = 1
+foreach ($r in $Requests)
+{
+    "Request {0} - Details" -f $counter
+    "`tRole:`t{0}" -f $r.RoleName
+    
+    # Get the requestor's display name from the Active Directory account
+    $RequestorDisplayName = ""
+    $RequestorDisplayName = (Get-ADUSer $r.Requestor).Name
+    
+    # Display the details of the request
+    "`tRequestor:`t{0} ({1})" -f $r.Requestor, $RequestorDisplayName
+    "`tJustification:`t{0})" -f $r.Justification
+    "`tStart Time:`t{0})" -f $r.RequestedTime
+    "`tLength of time requested:`t{0} hours" -f ($r.RequestedTTL/60/60)
+
+   # Get the approval action from the owner
+   $ApprovalAction = Get-ApprovalAction
+
+   switch ($ApprovalAction)
+   {
+       "approve" 
+       { 
+           "`n`tApproving";  
+           try
+           {
+               Set-PAMRequestToApprove -Request $r -Approve
+               "`n`tApproval Completed";  
+           }
+           catch 
+           {
+               Write-Warning "An error has occurred approving the request"
+           }
+       }
+       "deny" 
+       { 
+           "`n`tDenying" 
+            try
+            {
+                Set-PAMRequestToApprove -Request $r -Reject
+                "`n`tRejection completed";  
+            }
+            catch 
+            {
+                Write-Warning "An error has occurred rejecting the request"
+            }
+        }
+        "ignore" 
+        { 
+            "`n`tNo Action has been taken, the request can be rejected or approved at a later time"  
+        }
+    }
+}
