52829d791e63aeabd132684bb64d789936bd64b2
Project KB Blog <projectkbblog@outlook.com>
2018-10-24 08:39:30 +1000

    initial commit of scheduled backup scripts for MIM
    initial commit of scripts for backing up MIM configuration as a scheduled task.

---
+++ Scripts/Operational/Backup/MIM Service/BackupServiceConfigScheduled.ps1
@@ -0,0 +1,32 @@
+﻿param
+(
+    [Parameter(Mandatory=$true)]
+    [string]$DestinationFolder,
+    [switch]$CreateArchive,
+    [switch]$DeleteFiles
+)
+
+#$DestinationFolder = "E:\Backup\ServicePortal\"
+$DailyDirectory = ("{0}\{1}" -f $DestinationFolder, (Get-Date -format "yyyyMMdd"))
+$PolicyExportFile = ("{0}\policy.xml" -f $DailyDirectory)
+$SchemaExportFile = ("{0}\schema.xml" -f $DailyDirectory)
+
+New-Item -ItemType Directory -Path $DailyDirectory -ErrorAction SilentlyContinue
+
+.\ExportPolicy.ps1 -ExportFile $PolicyExportFile
+.\ExportSchema.ps1 -ExportFile $SchemaExportFile
+
+if ($CreateArchive)
+{
+    "Creating ZIP Archive of all exported files"
+    Add-Type -AssemblyName "System.IO.Compression.FileSystem"
+    
+    $ZipFile = ("{0}{1}_service_backup.zip" -f $DestinationFolder, (Get-Date -format "yyyyMMdd"))
+    [io.compression.zipfile]::CreateFromDirectory($DailyDirectory, $ZipFile)
+
+    if ($DeleteFiles)
+    {
+        Remove-Item -Path $DailyDirectory -Recurse -Confirm:$false
+
+    }
+}
---
+++ Scripts/Operational/Backup/MIM Sync Service/BackupSyncServiceFilesScheduled.ps1
@@ -0,0 +1,24 @@
+﻿param
+(
+    [Parameter(Mandatory=$true)]
+    [string]$DestinationFolder,
+    [switch]$CreateArchive,
+    [switch]$DeleteFiles
+)
+$DailyDirectory = ("{0}\{1}" -f $DestinationFolder, (Get-Date -format "yyyyMMdd"))
+
+#$ExportDirectory = ("E:\Backup\SyncService\{0}" -f (Get-Date -format "yyyyMMdd"))
+
+if (-not $CreateArchive)
+{
+    E:\Scripts\BackupSyncServiceFiles.ps1 -DestinationFolder $DailyDirectory 
+}
+elseif ($CreateArchive -and $DeleteFiles)
+{
+    E:\Scripts\BackupSyncServiceFiles.ps1 -DestinationFolder $DailyDirectory -CreateArchive -DeleteFiles
+}
+elseif ($CreateArchive -and (-not $DeleteFiles))
+{
+    E:\Scripts\BackupSyncServiceFiles.ps1 -DestinationFolder $DailyDirectory -CreateArchive
+}
+
