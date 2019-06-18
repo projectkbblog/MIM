f4168bdf7561da3d397185d46e58a82cbd67c1b4
Project KB Blog <projectkbblog@outlook.com>
2019-06-18 15:24:38 +1000

    initial verison of gruop creation scripts
    test version of group creation scripts.

---
+++ Scripts/Operational/MIM Portal Group Import/Functions.ps1
@@ -0,0 +1,88 @@
+﻿# Template for the group filter
+$GroupFilterTemplate = "<Filter xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" Dialect=""http://schemas.microsoft.com/2006/11/XPathFilterDialect"" xmlns=""http://schemas.xmlsoap.org/ws/2004/09/enumeration"">{0}</Filter>"
+
+function Get-UserByAccountName
+{
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $AccountName
+    )
+
+    return Get-Resource -ObjectType Person -AttributeName AccountName -AttributeValue $AccountName
+}
+
+function Does-GroupAlreadyExist
+{
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $DisplayName
+    )
+
+    try
+    {
+        $Group = Get-Resource -ObjectType Group -AttributeName DisplayName -AttributeValue $DisplayName -ErrorAction SilentlyContinue
+    }
+    catch
+    {
+        return $false
+    }
+
+    if ($Group)
+    {
+        return $true
+    }
+    else
+    {
+        return $false
+    }
+}
+
+function Is-GroupDynamic
+{
+    param
+    (
+        [Parameter(Mandatory=$false)]
+        [string] $MembershipLocked
+    )
+
+    if ($MembershipLocked -and ($MembershipLocked -eq "true"))
+    {
+        return $true
+    }
+    else
+    {
+        return $false
+    }
+}
+
+function Generate-GroupObject
+{
+    param
+    (
+        $GroupData
+    )
+    $newObject = New-Resource -ObjectType Group
+    $newObject.AccountName = $CurGroup.AccountName
+    $newObject.DisplayName = $CurGroup.DisplayName
+    $newObject.MailNickname =  $CurGroup.MailNickname
+    $newObject.Description = $CurGroup.Description
+    $newObject.Domain = $CurGroup.Domain
+    $newObject.MembershipAddWorkflow = $CurGroup.MembershipAddWorkflow
+    $newObject.MembershipLocked = $DynamicGroup
+    $newObject.Scope = $CurGroup.Scope
+    $newObject.Type = $CurGroup.Type
+
+    if ($Owner)
+    {
+        $newObject.Owner = $Owner.ObjectID.Value;
+    }
+    if ($DisplayedOwner)
+    {
+        $newObject.DisplayedOwner = $DisplayedOwner.ObjectID.Value;
+    }
+    $newObject.Filter = ($GroupFilterTemplate -f $CurGroup.Filter)
+
+    return $newObject
+}
---
+++ Scripts/Operational/MIM Portal Group Import/Import-MIMPortalGroups.ps1
@@ -0,0 +1,61 @@
+﻿param
+(
+    [Parameter(Mandatory=$false)]
+    [string] $MIMServiceHost,
+    [Parameter(Mandatory=$true)]
+    [string] $GroupsFile
+)
+
+if (-not $MIMServiceHost)
+{
+    $MIMServiceHost = "localhost"    
+}
+. .\Functions.ps1
+
+try
+{
+    Import-Module LithnetRMA
+}
+catch
+{
+    Write-Warning "The LithnetRMA module is not installed, please download and install it from 'https://github.com/lithnet/resourcemanagement-powershell/wiki/installing-the-module'"
+    exit -1
+}
+
+# Define the MIM Service Host address
+Set-ResourceManagementClient -BaseAddress ("http://{0}:5725" -f $MIMServiceHost)
+
+# Import the groups file
+"Reading Group data from {0}" -f $GroupsFile
+$GroupsCsv = Import-CSV -Path $GroupsFile
+$counter = 1
+foreach($CurGroup in $GroupsCsv)
+{
+    "`n{0} - {1} - Processing the Group '{2}'" -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss"), $counter, $CurGroup.DisplayName
+
+    # If the group is dynamic then get the owner/displayed owner
+    $DynamicGroup = Is-GroupDynamic -MembershipLocked $CurGroup.MembershipLocked
+    if ($DynamicGroup)
+    {
+        "`tResolving group owner references"
+        # Get the reference for the group owner
+        $Owner =  Get-UserByAccountName -AccountName $CurGroup.Owner
+        $DisplayedOwner = Get-UserByAccountName -AccountName $CurGroup.DisplayedOwner
+    }
+
+    if (Does-GroupAlreadyExist -DisplayName $CurGroup.DisplayName)
+    {
+        "`tGroup already exists - no action being taken"
+    }
+    else
+    {
+        "`tGroup doesn't yet exit - creating the group"
+        # Generate the group object
+        $NewGroup = Generate-GroupObject -GroupData $CurGroup 
+
+        # SAve the resource to the MIM Service (this is a LithnetRMA cmdlet)
+        Save-Resource $NewGroup    
+        "`tGroup created"
+    }
+    $counter++
+}
